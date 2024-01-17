d3f47a20a8e1137bb43d3e24288f8aca11018523
Matthew Balkwill <mabalkwi@microsoft.com>
2024-01-17 12:23:14 +0000

    Fix steps to install tcppump

--- network/execution/run_it.sh
+++ network/execution/run_it.sh
@@ -3,29 +3,32 @@
 
 if [ ! -d "tcppump" ]; then
     echo "Creating tcppump."
+    sudo tdnf install libpcap
+    sudo tdnf install libpcap-devel
     git clone https://github.com/amartin755/tcppump.git
     pushd .
     cd tcppump
-    if [ ! -d "build" ]; then
-        rm -rf build/*
-    fi
+    rm -rf build/*
     cmake -B build
     cmake --build build
     popd
 fi
 
-labru1_ru_fh_mac="6c:ad:ad:00:00:69"
-labru1_server_fh_mac="66:44:33:22:11:00"
+labru1_ru_fh_mac="6c:ad:ad:00:08:86"
+labru1_server_fh_mac="00:11:22:33:44:66"
+
+labintru1_ru_fh_mac="6c:ad:ad:00:00:69"
+labintru1_server_fh_mac="66:44:33:22:11:00"
 
 devel5g3_server_fh_intf=eth1      # find this using sudo ip link
 
-telco2_server_fh_intf=eth4      # find this using sudo ip link
-#telco2_server_fh_mac="aa:bb:cc:dd:ee:ff"
+telco2_server_fh_intf=eth5      # find this using sudo ip link
+
 
 # CHANGE THE FOLLOWING 3 LINES AS NEEDED
-server_fh_intf=
-server_fh_mac=
-ru_fh_mac="11:11:11:11:aa:bb"                  # random
+server_fh_intf=$telco2_server_fh_intf
+server_fh_mac=$labru1_server_fh_mac
+ru_fh_mac=$labru1_ru_fh_mac
 
 delay=1
 count=16
